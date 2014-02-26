b9522f6827d850f6578a2147550a84366ecd0786
stu43005 <stu43005@gmail.com>
2014-02-26 20:53:09 +0800

    分析Raw\kcsapi_master_slotitem

--- Grabacr07.KanColleWrapper/Models/Raw/kcsapi_master_slotitem.cs
+++ Grabacr07.KanColleWrapper/Models/Raw/kcsapi_master_slotitem.cs
@@ -14,24 +14,24 @@ namespace Grabacr07.KanColleWrapper.Models.Raw
 		public string api_name { get; set; }
 		public int[] api_type { get; set; }
 		public int api_taik { get; set; }
-		public int api_souk { get; set; }
-		public int api_houg { get; set; }
-		public int api_raig { get; set; }
+		public int api_souk { get; set; } // 装甲
+		public int api_houg { get; set; } // 火力
+		public int api_raig { get; set; } // 雷装
 		public int api_soku { get; set; }
-		public int api_baku { get; set; }
-		public int api_tyku { get; set; }
-		public int api_tais { get; set; }
+		public int api_baku { get; set; } // 爆装
+		public int api_tyku { get; set; } // 対空
+		public int api_tais { get; set; } // 対潜
 		public int api_atap { get; set; }
-		public int api_houm { get; set; }
-		public int api_raim { get; set; }
-		public int api_houk { get; set; }
+		public int api_houm { get; set; } // 命中
+		public int api_raim { get; set; } // 
+		public int api_houk { get; set; } // 回避
 		public int api_raik { get; set; }
 		public int api_bakk { get; set; }
-		public int api_saku { get; set; }
+		public int api_saku { get; set; } // 索敵
 		public int api_sakb { get; set; }
 		public int api_luck { get; set; }
-		public int api_leng { get; set; }
-		public int api_rare { get; set; }
+		public int api_leng { get; set; } // 射程: -,短,中,長,超長
+		public int api_rare { get; set; } // 
 		public int[] api_broken { get; set; }
 		public string api_info { get; set; }
 		public string api_usebull { get; set; }
