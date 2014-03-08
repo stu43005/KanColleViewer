06a7e7be02d5da6f9433f86a6de0274dca1999e4
stu43005 <stu43005@gmail.com>
2014-03-08 13:56:50 +0800

    艦隊概觀使用滑鼠滾輪切換

---
+++ Grabacr07.KanColleViewer/Views/Behaviors/MouseWheelFleetsAction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interactivity;
+using Grabacr07.KanColleViewer.ViewModels.Contents.Fleets;
+
+namespace Grabacr07.KanColleViewer.Views.Behaviors
+{
+	class MouseWheelFleetsAction : TargetedTriggerAction<FleetsViewModel>
+	{
+		public bool IsCyclic { get; set; }
+
+		protected override void Invoke(object parameter)
+		{
+			if (!(parameter is int))
+				return;
+
+			var Delta = (int)parameter;
+			if (Delta < 0)
+			{
+				Next();
+			}
+			else
+			{
+				Previous();
+			}
+		}
+
+		private void Next()
+		{
+			if (this.Target == null)
+				return;
+
+			var selectedIndex = Array.IndexOf(this.Target.Fleets, this.Target.SelectedFleet);
+			if (selectedIndex < this.Target.Fleets.Length - 1)
+				this.Target.SelectedFleet = this.Target.Fleets[selectedIndex + 1];
+			else if (this.IsCyclic)
+				this.Target.SelectedFleet = this.Target.Fleets.First();
+		}
+
+		private void Previous()
+		{
+			if (this.Target == null)
+				return;
+
+			var selectedIndex = Array.IndexOf(this.Target.Fleets, this.Target.SelectedFleet);
+			if (selectedIndex > 0)
+				this.Target.SelectedFleet = this.Target.Fleets[selectedIndex - 1];
+			else if (this.IsCyclic)
+				this.Target.SelectedFleet = this.Target.Fleets.Last();
+		}
+	}
+}
---
+++ Grabacr07.KanColleViewer/Views/Behaviors/MouseWheelTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Grabacr07.KanColleViewer.Views.Behaviors
+{
+	class MouseWheelTrigger : TriggerBase<UIElement>
+	{
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+			this.AssociatedObject.MouseWheel += OnMouseWheel;
+		}
+
+		protected override void OnDetaching()
+		{
+			this.AssociatedObject.MouseWheel -= OnMouseWheel;
+			base.OnDetaching();
+		}
+
+		private void OnMouseWheel(object sender, MouseWheelEventArgs e)
+		{
+			this.InvokeActions(e.Delta);
+		}
+	}
+}
