d3f1f7a02ddc3efdd4d3b654fb94a39c68294f9b
stu43005 <stu43005@gmail.com>
2015-05-19 22:08:29 +0800

    無解，分了。
    修正編譯報錯：『c:\Program Files (x86)\Windows
    Kits\8.1\References\CommonConfiguration\Neutral\Windows.winmd : error
    CS0012: 型別 'System.Attribute' 是定義在未參考的組件中。您必須加入參考至組件 'System.Runtime,
    Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'。』

---
+++ Grabacr07.KanColleViewer/ViewModels/Catalogs/SortableColumnViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Grabacr07.Desktop.Metro.Controls;
+
+namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
+{
+	class SortableColumnViewModel
+	{
+		// 無解，分了。
+		private SortDirection _Direction;
+	}
+}
